f68cd61ddbf8ba34f2a17f18374175aaeb052fe1
Abbie Weisenbloom <abbiew@froggie.com>
2018-09-03 22:55:19 -0700

    Added player & add class names instead of prototype

--- js/app.js
+++ js/app.js
@@ -1,35 +1,51 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+class Enemy {
+    constructor(x, y) {
+        this.sprite = 'images/enemy-bug.png';
+        this.x = x;
+        this.y = y;
+    }
+    update() { //methods inherited by all instances
+        console.log('starting engines…');
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+class Player {
+    constructor(x, y) {
+        this.sprite = 'images/char-boy.png';
+        this.x = x;
+        this.y = y;
+    }
+    update(dt) { //methods inherited by all instances
+        console.log('starting player update');
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const enemy1 = new Enemy(0, 50);
+const enemy2 = new Enemy(0, 150);
+const enemy3 = new Enemy(0, 230);
+const enemy4 = new Enemy(0, 310);
 
+const allEnemies = [enemy1, enemy2, enemy3, enemy4];
+const player = new Player(0, 400);
 
 
 // This listens for key presses and sends the keys to your
