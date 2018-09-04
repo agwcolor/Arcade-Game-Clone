9f27bfbc14f600ddac2d0c36979cdd30614a41cb
Abbie Weisenbloom <abbiew@froggie.com>
2018-09-04 11:43:28 -0700

    Switch statment added for the player to move

--- js/app.js
+++ js/app.js
@@ -5,9 +5,11 @@ class Enemy {
         this.sprite = 'images/enemy-bug.png';
         this.x = x;
         this.y = y;
+        this.speed = Math.floor(Math.random() * 8 + 1);
     }
-    update() { //methods inherited by all instances
-        console.log('starting engines…');
+    update(dt) { //methods inherited by all instances
+        //console.log('starting update');
+        this.x += this.speed * dt;
     }
 
     render() {
@@ -27,36 +29,61 @@ class Player {
         this.y = y;
     }
     update(dt) { //methods inherited by all instances
-        console.log('starting player update');
-    }
+        //console.log('starting player update');
+        allEnemies.forEach((enemy) => {
+                if (enemy.x < this.x + 70 && enemy.x + 70 > this.x && enemy.y < this.y + 70 && enemy.y + 70 > this.y) {
+                    this.reset();
+                }
+            });
+        }
 
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        }
 
+    handleInput(key) {
+        //console.log(key + " is the key!");
+            switch (key) {
+                case 'left':
+                    this.x -= 90;
+                    break;
+                case 'down':
+                    this.y += 80;
+                    break;
+                case 'right':
+                    this.x += 80;
+                    break;
+                case 'up':
+                    this.y -= 80;
+                    break;
+            }
     }
-}
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-const enemy1 = new Enemy(0, 50);
-const enemy2 = new Enemy(0, 150);
-const enemy3 = new Enemy(0, 230);
-const enemy4 = new Enemy(0, 310);
+    reset() {
+        console.log("I'm in the reset function");
+        }
+};
 
-const allEnemies = [enemy1, enemy2, enemy3, enemy4];
-const player = new Player(0, 400);
+    // Now instantiate your objects.
+    // Place all enemy objects in an array called allEnemies
+    // Place the player object in a variable called player
+    const enemy1 = new Enemy(0, 50);
+    const enemy2 = new Enemy(0, 150);
+    const enemy3 = new Enemy(0, 230);
+    const enemy4 = new Enemy(0, 310);
+
+    const allEnemies = [enemy1, enemy2, enemy3, enemy4];
+    const player = new Player(0, 400);
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
-    player.handleInput(allowedKeys[e.keyCode]);
-});
+    // This listens for key presses and sends the keys to your
+    // Player.handleInput() method. You don't need to modify this.
+    document.addEventListener('keydown', function(e) {
+        var allowedKeys = {
+            37: 'left',
+            38: 'up',
+            39: 'right',
+            40: 'down'
+        };
+        player.handleInput(allowedKeys[e.keyCode]);
+    });
